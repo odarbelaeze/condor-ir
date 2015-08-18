f3f30b80055ae42567d9e4174f2a585738bf2747
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2015-08-18 16:55:57 -0500

    Doing some progress.

--- metadata.py
+++ metadata.py
@@ -1,87 +1,124 @@
 import bisect
-import glob
 import contextlib
+import glob
+import itertools
 
 import pymongo
 
 from xml.dom import minidom
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
 
 
-STOPWORDS = stopwords.words()
+STOPWORDS = sorted(stopwords.words())
 # http://www.talkpythontome.com/episodes/show/2/python-and-mongodb
 # Don't be afraid of creating a global for the mongo client
 MONGO_CLIENT = pymongo.MongoClient('localhost', 27017)
+STEMER = SnowballStemmer('spanish')
 
 
-def title(record):
-    title = record.getElementsByTagName('lom:title').item(0)
-    if title is None:
-        return ''
-    return title.firstChild.nodeValue
+class Record(object):
 
+    def __init__(self, dom_element):
+        self.dom_element = dom_element
 
-def keywords(record):
-    keywords = record.getElementsByTagName('lom:keyword')
-    if keywords.length == 0:
-        return ''
-    return ' '.join([keyword.firstChild.nodeValue
-                     for keyword in keywords])
+    @property
+    def title(self):
+        title = self.dom_element.getElementsByTagName('lom:title').item(0)
+        if title is None:
+            return ''
+        return title.firstChild.nodeValue
 
+    @property
+    def keywords(self):
+        keywords = self.dom_element.getElementsByTagName('lom:keyword')
+        if keywords.length == 0:
+            return ''
+        return ' '.join([keyword.firstChild.nodeValue
+                         for keyword in keywords])
 
-def description(record):
-    description = record.getElementsByTagName('lom:description').item(0)
-    if description is None:
-        return ''
-    return description.firstChild.nodeValue
+    @property
+    def description(self):
+        description = self.dom_element.getElementsByTagName('lom:description')
+        description = description.item(0)
+        if description is None:
+            return ''
+        return description.firstChild.nodeValue
 
+    @property
+    def xml(self):
+        return self.dom_element.toxml()
 
-def metadata(filename):
-    dom = minidom.parse(filename)
-    record_id = -1
-    for record in dom.getElementsByTagName('record'):
-        record_id += 1
-        yield {
-            'title': title(record),
-            'keywords': keywords(record),
-            'description': description(record),
-            'raw': record.toxml(),
-            'id': record_id,
+    @property
+    def metadata(self):
+        return {
+            'title': self.title,
+            'keywords': self.keywords,
+            'description': self.description,
+            'xml': self.xml,
         }
+
+    def raw_data(self, fields=None):
+        return raw_data(self.metadata, fields)
 
 
 def is_stopword(word):
-    return bisect.bisect_left(STOPWORDS, word) < len(STOPWORDS)
-    # return word in STOPWORDS
+    i = bisect.bisect_left(STOPWORDS, word.lower())
+    if i < len(STOPWORDS) and word == STOPWORDS[i]:
+        return True
+    return False
+
+
+def is_valid(word):
+    return not (is_stopword(word) or word.isspace()) and word.isalnum()
+
+
+def normalize(word):
+    return STEMER.stem(word.lower())
 
 
 def raw_data(meta, interest_fields=None):
     _interest_fields = interest_fields or ['title', 'keywords', 'description']
     tokens = []
     for key in _interest_fields:
-        tokens.extend(meta[key].split(' '))     # this will change
-    return filter(is_stopword, tokens)
+        key_tokens = word_tokenize(meta[key], language='spanish')
+        tokens.extend(key_tokens)
+    return filter(is_valid, map(normalize, tokens))
+
+
+def metadata(filename):
+    dom = minidom.parse(filename)
+    for dom_element in dom.getElementsByTagName('record'):
+        record = Record(dom_element)
+        yield record.metadata
 
 
 @contextlib.contextmanager
-def collection(delete=True):
-    database = MONGO_CLIENT['lsa-froac']
+def collection(dbname='lsa-froac', delete=True):
+    database = MONGO_CLIENT[dbname]
     records = database['records']
-    records.create_index([('id', pymongo.ASCENDING, ), ])
     if delete:
         records.delete_many({})
     yield records
 
 
+def word_set():
+    def words():
+        with collection(delete=False) as records:
+            for record in records.find():
+                yield raw_data(record)
+    return sorted(set(itertools.chain.from_iterable(words())))
+
+
 def main():
     with collection() as records:
-        for filename in glob.glob('data/*.xml'):
+        for filename in glob.glob('data/**/*.xml'):
             records.insert_many(
                 list(metadata(filename)))
 
-    with collection(delete=False) as records:
-        for record in records.find():
-            print(' '.join(raw_data(record)))
+    for word in word_set():
+        print(word)
 
 if __name__ == '__main__':
     main()
