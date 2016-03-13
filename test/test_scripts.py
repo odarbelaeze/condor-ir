ebbcd8192c97dd8b3743704b22af1d060c34e479
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2016-03-13 17:53:05 -0500

    Adding tests skeletons for the script

---
+++ test/test_scripts.py
@@ -0,0 +1,30 @@
+import pytest
+
+from click.testing import CliRunner
+
+from lsa.scripts.populate import lsapopulate
+from lsa.scripts.model import lsamodel
+from lsa.scripts.query import lsaquery
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+def test_basic_usage(runner):
+    res = runner.invoke(lsapopulate, [])
+    assert res.exit_code == 2
+    assert 'Usage' in res.output
+
+
+def test_basic_usage_model(runner):
+    res = runner.invoke(lsamodel, ['--help'])
+    assert res.exit_code == 0
+    assert 'Usage' in res.output
+
+
+def test_basic_usagle_query(runner):
+    res = runner.invoke(lsaquery, [])
+    assert res.exit_code == 2
+    assert 'Usage' in res.output
