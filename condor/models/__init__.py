324e16250a2c5e35a296a7894b6700a39ca6ad3d
Oscar David Arbeláez <odarbelaeze@gmail.com>
2016-06-26 17:13:12 -0500

    Expand models on their own files.

---
+++ condor/models/__init__.py
@@ -0,0 +1,4 @@
+from .bibliography import Bibliography
+from .bibliography_set import BibliographySet
+from .term_document_matrix import TermDocumentMatrix
+from .ranking_matrix import RankingMatrix
