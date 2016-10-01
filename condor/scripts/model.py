b21ec4097329f73ec6368a085eda1cddf4ff4235
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2016-10-01 16:51:23 -0500

    Move stuff out of model

--- condor/scripts/model.py
+++ condor/scripts/model.py
@@ -3,9 +3,7 @@ This script aims to build the model for the current state of a database,
 taking in all the present records.
 '''
 
-import collections
 import hashlib
-import itertools
 import os
 import sys
 
@@ -13,52 +11,17 @@ import click
 import numpy
 
 from condor.dbutil import session
+from condor.builders.matrix import build_matrix
 from condor.models import (
     BibliographySet,
     TermDocumentMatrix,
     RankingMatrix
 )
-from condor.normalize import CompleteNormalizer
-
-
-def get_tokens(record, fields=None, list_fields=None):
-    fields = fields or ['title', 'description', ]
-    list_fields = list_fields or ['keywords', ]
-    tokens = []
-    for field in fields:
-        tokens.extend(record.get(field, '').split())
-    for field in list_fields:
-        value = record.get(field, '{""}')
-        if value == '{""}':
-            continue
-        tokens.extend(value[1:-1].split(','))
-    return tokens
-
-
-def raw_data(bib):
-    normalizer = CompleteNormalizer(language=bib.language)
-    return [normalizer.apply_to(token) for token in get_tokens(bib.__dict__)]
-
-
-def word_set(bibset):
-    def words():
-        for bib in bibset.bibliographies:
-            yield raw_data(bib)
-    return sorted(set(itertools.chain.from_iterable(words())))
-
-
-def records(bibset):
-    for bib in bibset.bibliographies:
-        yield bib.eid
-
-
-def matrix(bibset, words):
-    word_dict = {word: pos for pos, word in enumerate(words)}
-    for ind, bib in enumerate(bibset.bibliographies):
-        raw = list(raw_data(bib))
-        frequency = collections.Counter(raw)
-        for word, freq in frequency.items():
-            yield ind, word_dict[word], freq
+from condor.config import (
+    TERM_LIST_PATH,
+    MATRIX_PATH,
+    MODEL_PATH,
+)
 
 
 @click.group()
@@ -97,30 +60,15 @@ def create(target, verbose):
     click.echo('I will generate a model for the {} bibset...'.format(
         bibset.eid))
 
-    words = word_set(bibset)
-    nwords = len(words)
-    click.echo('I found {} unique word toquens in the database...'.format(
-        len(words)))
-
-    ndocs = len(bibset.bibliographies)
-    click.echo(
-        'I found {} unique documents toquens in the database...'.format(
-            ndocs
-        )
-    )
-
-    frequency = numpy.zeros((ndocs, nwords), dtype=int)
-    for row, col, freq in matrix(bibset, words):
-        frequency[row, col] = freq
+    words, frequency, options = build_matrix(bibset)
+    nwords, nrecs = frequency.shape
 
     if verbose:
-        nwords, nrecs = matrix.shape
         click.echo('I\'ve built the frequency matrix with the folowing traits')
         click.echo('Number of records: {}'.format(nrecs))
         click.echo('Number of words: {}'.format(nwords))
 
     U, S, V = numpy.linalg.svd(frequency, full_matrices=False)
-
     ss = S / numpy.sum(S)
     ss = numpy.cumsum(ss)
 
@@ -136,41 +84,31 @@ def create(target, verbose):
         click.echo('Number of records: {}'.format(nrecs))
         click.echo('Number of words: {}'.format(nwords))
 
-    if not os.path.exists('matrices'):
-        os.mkdir('matrices')
-
     matrix_hash = hashlib.sha1(
-        '{}{}{}{}'.format(bibset.eid, bibset.modified, ndocs, nwords)
+        '{}{}{}{}'.format(bibset.eid, bibset.modified, nrecs, nwords)
         .encode()
     ).hexdigest()
-
-    matrix_filename = os.path.join('matrices', '{}.npy'.format(matrix_hash))
+    matrix_filename = os.path.join(MATRIX_PATH, '{}.npy'.format(matrix_hash))
     click.echo(
         'Storing the term document matrix at {}'
         .format(matrix_filename)
     )
-    numpy.save(matrix_filename, matrix)
+    numpy.save(matrix_filename, frequency)
 
-    if not os.path.exists('term_lists'):
-        os.mkdir('term_lists')
-
-    term_list_path = os.path.join('term_lists', '{}.txt'.format(matrix_hash))
-    click.echo(
-        'Storing the term list at {}'
-        .format(term_list_path)
+    term_list_filename = os.path.join(
+        TERM_LIST_PATH, '{}.txt'.format(matrix_hash)
     )
-    with open(term_list_path, 'w') as file:
+    click.echo(
+        'Storing the term list at {}'.format(TERM_LIST_PATH)
+    )
+    with open(term_list_filename, 'w') as file:
         file.write('\n'.join(words))
 
-    if not os.path.exists('models'):
-        os.mkdir('models')
-
     model_hash = hashlib.sha1(
-        '{}{}{}{}condor'.format(bibset.eid, bibset.modified, ndocs, nwords)
+        '{}{}{}{}condor'.format(bibset.eid, bibset.modified, nrecs, nwords)
         .encode()
     ).hexdigest()
-
-    model_filename = os.path.join('models', '{}.npy'.format(model_hash))
+    model_filename = os.path.join(MODEL_PATH, '{}.npy'.format(model_hash))
     click.echo(
         'Storing the ranking matrix at {}'
         .format(model_filename)
@@ -178,8 +116,8 @@ def create(target, verbose):
     numpy.save(model_filename, acoted)
     td_matrix = TermDocumentMatrix(
         bibliography_options='',
-        processing_options=str(CompleteNormalizer.__mro__),
-        term_list_path=term_list_path,
+        processing_options=options,
+        term_list_path=term_list_filename,
         tdidf_matrix_path=matrix_filename,
     )
     td_matrix.bibliography_set = bibset
