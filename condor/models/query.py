06a2780b6e9b246675b3d1aada7d0fd4b61b7159
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-02-18 16:14:34 -0500

    Add the query and query result models

---
+++ condor/models/query.py
@@ -0,0 +1,54 @@
+from sqlalchemy import Column, ForeignKey, Unicode
+from sqlalchemy.orm import relationship
+
+from condor.models.base import AuditableMixing, DeclarativeBase
+
+
+class Query(AuditableMixing, DeclarativeBase):
+
+    __tablename__ = 'query'
+
+    bibliography_set_eid = Column(
+        Unicode(40),
+        ForeignKey('bibliography_set.eid')
+    )
+
+    contributor = Column(
+        Unicode(40),
+        nullable=True
+    )
+
+    query_string = Column(
+        Unicode(),
+        nullable=False
+    )
+
+    bibliography_set = relationship(
+        'BibliographySet',
+        back_populates='queries'
+    )
+
+
+class QueryResult(AuditableMixing, DeclarativeBase):
+
+    __tablename__ = 'query_result'
+
+    query_eid = Column(
+        Unicode(40),
+        ForeignKey('query.eid')
+    )
+
+    bibliography_eid = Column(
+        Unicode(40),
+        ForeignKey('bibliography.eid')
+    )
+
+    query = relationship(
+        'Query',
+        back_populates='results'
+    )
+
+    bibliography = relationship(
+        'Bibliography',
+        back_populates='query_results'
+    )
