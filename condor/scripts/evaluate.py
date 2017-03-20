12a4e5732e52f72e2f77c7791cfdb47653bf715e
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-03-19 19:10:01 -0500

    Count the results

--- condor/scripts/evaluate.py
+++ condor/scripts/evaluate.py
@@ -16,9 +16,25 @@ def evaluate(db, target, limit):
     precision and recall values for the different queries and an average of
     these values at the end.
     """
-    rankin_matrix = find_one(db, RankingMatrix, target)
-    queries = rankin_matrix.term_document_matrix.bibliography_set.queries
+    ranking_matrix = find_one(db, RankingMatrix, target)
+    bibliography_set = ranking_matrix.term_document_matrix.bibliography_set
+    queries = bibliography_set.queries
+    universe = set(d.eid for d in bibliography_set.bibliographies)
 
     for query in queries:
-        results = rankin_matrix.query(query.query_string.split(), limit=limit)
-        print(results)
+        results = ranking_matrix.query(query.query_string.split(), limit=limit)
+        experiment = set(r.eid for r, _ in results)
+        truth = set(r.bibliography.eid for r in query.results)
+        false_negatives = truth.difference(experiment)
+        true_positives = truth.intersection(experiment)
+        false_positives = experiment.difference(truth)
+        true_negatives = universe.difference(truth.union(experiment))
+        print(
+            query.query_string,
+            '\n',
+            'FN', len(false_negatives),
+            'TP', len(true_positives),
+            'FP', len(false_positives),
+            'TN', len(true_negatives),
+            '\n',
+        )
